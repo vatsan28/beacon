60d0d87063d42424c1c813bc62086e9f5b89728e
vatsan28 <smural21@uic.edu>
2017-04-15 13:28:19 -0500

    Added login and basic registration

--- config/vars.js
+++ config/vars.js
@@ -1,6 +1,6 @@
 module.exports = {
 
     'secret': 'terriblesessionpassword',
-    // 'database': 'mongodb://localhost/innovationLab_db' // For a local database
-    'database':'mongodb://grainger:grainger123@ds153815.mlab.com:53815/smartvend' // For a remote database.
+    'database': 'mongodb://localhost/innovationLab_db' // For a local database
+    // 'database':'mongodb://grainger:grainger123@ds153815.mlab.com:53815/smartvend' // For a remote database.
 };
