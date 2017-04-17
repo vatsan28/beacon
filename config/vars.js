2d322742480c41638bb022833809bffca279f343
vatsan28 <smural21@uic.edu>
2017-04-17 00:51:11 -0500

    Completed registration client side

--- config/vars.js
+++ config/vars.js
@@ -1,9 +1,5 @@
 module.exports = {
 
     'secret': 'terriblesessionpassword',
-<<<<<<< HEAD
     'database':'mongodb://beacon:beacon123@ds161630.mlab.com:61630/beacon' // For a remote database.
-=======
-   'database':'mongodb://beacon:beacon123@ds161630.mlab.com:61630/beacon' // For a remote database.
->>>>>>> 93db38388d5d278016785b163583dd237f9235ac
 };
